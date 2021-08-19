82c710c64f619f22efcfd35cc196242c410a4d68
Rik Schennink <rikschennink@gmail.com>
2021-08-19 10:00:08 +0200

    fix demo

--- src/js/index.js
+++ src/js/index.js
@@ -8,21 +8,21 @@ const plugin = _ => {
     const { Type, createRoute, createItemAPI = item => item } = utils;
     const { fileActionButton } = views;
 
-    addFilter('SHOULD_REMOVE_ON_REVERT', (shouldRemove, { item, query }) => 
-        new Promise(resolve => {
+    addFilter(
+        'SHOULD_REMOVE_ON_REVERT',
+        (shouldRemove, { item, query }) =>
+            new Promise(resolve => {
+                const { file } = item;
 
-            const { file } = item;
+                // if this file is editable it shouldn't be removed immidiately even when instant uploading
+                const canEdit =
+                    query('GET_ALLOW_IMAGE_EDIT') &&
+                    query('GET_IMAGE_EDIT_ALLOW_EDIT') &&
+                    isPreviewableImage(file);
 
-            // if this file is editable it shouldn't be removed immidiately even when instant uploading
-            const canEdit = 
-                query('GET_ALLOW_IMAGE_EDIT') &&
-                query('GET_IMAGE_EDIT_ALLOW_EDIT') &&
-                isPreviewableImage(file);
-
-            // if the file cannot be edited it should be removed on revert
-            resolve(!canEdit);
-
-        })
+                // if the file cannot be edited it should be removed on revert
+                resolve(!canEdit);
+            })
     );
 
     // open editor when loading a new item
@@ -30,7 +30,6 @@ const plugin = _ => {
         'DID_LOAD_ITEM',
         (item, { query, dispatch }) =>
             new Promise((resolve, reject) => {
-
                 // if is temp or local file
                 if (item.origin > 1) {
                     resolve(item);
@@ -41,7 +40,8 @@ const plugin = _ => {
                 const { file } = item;
                 if (
                     !query('GET_ALLOW_IMAGE_EDIT') ||
-                    !query('GET_IMAGE_EDIT_INSTANT_EDIT')) {
+                    !query('GET_IMAGE_EDIT_INSTANT_EDIT')
+                ) {
                     resolve(item);
                     return;
                 }
@@ -52,77 +52,77 @@ const plugin = _ => {
                     return;
                 }
 
-                const createEditorResponseHandler = (item, resolve, reject) => (userDidConfirm) => {
-
+                const createEditorResponseHandler = (
+                    item,
+                    resolve,
+                    reject
+                ) => userDidConfirm => {
                     // remove item
                     editRequestQueue.shift();
-                    
+
                     // handle item
                     if (userDidConfirm) {
                         resolve(item);
-                    }
-                    else {
+                    } else {
                         reject(item);
                     }
 
                     // TODO: Fix, should not be needed to kick the internal loop in case no processes are running
                     dispatch('KICK');
-                    
+
                     // handle next item!
                     requestEdit();
-                }
+                };
 
                 const requestEdit = () => {
-                    
                     if (!editRequestQueue.length) return;
-                    
-                    const {item, resolve, reject} = editRequestQueue[0];
-                    
+
+                    const { item, resolve, reject } = editRequestQueue[0];
+
                     dispatch('EDIT_ITEM', {
-                        id: item.id, 
-                        handleEditorResponse: createEditorResponseHandler(item, resolve, reject)
+                        id: item.id,
+                        handleEditorResponse: createEditorResponseHandler(
+                            item,
+                            resolve,
+                            reject
+                        ),
                     });
-                }
+                };
 
                 queueEditRequest({ item, resolve, reject });
 
                 if (editRequestQueue.length === 1) {
                     requestEdit();
                 }
-
             })
     );
 
-
     // extend item methods
-    addFilter(
-        'DID_CREATE_ITEM', (item, { query, dispatch }) => {
-            item.extend('edit', () => {
-                dispatch('EDIT_ITEM', { id: item.id })
-            });
-        }
-    );
-
+    addFilter('DID_CREATE_ITEM', (item, { query, dispatch }) => {
+        item.extend('edit', () => {
+            dispatch('EDIT_ITEM', { id: item.id });
+        });
+    });
 
     const editRequestQueue = [];
-    const queueEditRequest = (editRequest) => {
+    const queueEditRequest = editRequest => {
         editRequestQueue.push(editRequest);
         return editRequest;
-    }
+    };
 
     // called for each view that is created right after the 'create' method
     addFilter('CREATE_VIEW', viewAPI => {
-
         // get reference to created view
         const { is, view, query } = viewAPI;
 
         if (!query('GET_ALLOW_IMAGE_EDIT')) return;
 
         const canShowImagePreview = query('GET_ALLOW_IMAGE_PREVIEW');
-        
+
         // only run for either the file or the file info panel
-        const shouldExtendView = (is('file-info') && !canShowImagePreview) ||
-                                 (is('file') && canShowImagePreview);
+        const shouldExtendView =
+            (is('file-info') && !canShowImagePreview) ||
+            (is('file') && canShowImagePreview);
 
         if (!shouldExtendView) return;
 
@@ -136,20 +136,23 @@ const plugin = _ => {
             editor.outputFile = false;
             editor.filepondCallbackBridge = {
                 onconfirm: editor.onconfirm || (() => {}),
-                oncancel: editor.oncancel || (() => {})
-            }
+                oncancel: editor.oncancel || (() => {}),
+            };
         }
 
         // opens the editor, if it does not already exist, it creates the editor
         const openEditor = ({ root, props, action }) => {
-
             const { id } = props;
             const { handleEditorResponse } = action;
-            
+
             // update editor props that could have changed
-            editor.cropAspectRatio = root.query('GET_IMAGE_CROP_ASPECT_RATIO') || editor.cropAspectRatio;
-            editor.outputCanvasBackgroundColor = root.query('GET_IMAGE_TRANSFORM_CANVAS_BACKGROUND_COLOR') || editor.outputCanvasBackgroundColor;
-            
+            editor.cropAspectRatio =
+                root.query('GET_IMAGE_CROP_ASPECT_RATIO') ||
+                editor.cropAspectRatio;
+            editor.outputCanvasBackgroundColor =
+                root.query('GET_IMAGE_TRANSFORM_CANVAS_BACKGROUND_COLOR') ||
+                editor.outputCanvasBackgroundColor;
+
             // get item
             const item = root.query('GET_ITEM', id);
             if (!item) return;
@@ -161,16 +164,16 @@ const plugin = _ => {
             const crop = item.getMetadata('crop');
             const cropDefault = {
                 center: {
-                    x: .5,
-                    y: .5
+                    x: 0.5,
+                    y: 0.5,
                 },
                 flip: {
                     horizontal: false,
-                    vertical: false
+                    vertical: false,
                 },
                 zoom: 1,
                 rotation: 0,
-                aspectRatio: null
+                aspectRatio: null,
             };
 
             // size data to pass to editor
@@ -185,22 +188,26 @@ const plugin = _ => {
             // build parameters object
             const imageParameters = {
                 crop: crop || cropDefault,
-                size: resize ? {
-                    upscale: resize.upscale,
-                    mode: resize.mode,
-                    width: resize.size.width,
-                    height: resize.size.height
-                } : null,
-                filter: filters ? filters.id || filters.matrix : 
-                    (root.query('GET_ALLOW_IMAGE_FILTER') && root.query('GET_IMAGE_FILTER_COLOR_MATRIX') && !colors) 
-                        ? filter 
-                        : null,
+                size: resize
+                    ? {
+                          upscale: resize.upscale,
+                          mode: resize.mode,
+                          width: resize.size.width,
+                          height: resize.size.height,
+                      }
+                    : null,
+                filter: filters
+                    ? filters.id || filters.matrix
+                    : root.query('GET_ALLOW_IMAGE_FILTER') &&
+                      root.query('GET_IMAGE_FILTER_COLOR_MATRIX') &&
+                      !colors
+                    ? filter
+                    : null,
                 color: colors,
-                markup
+                markup,
             };
 
             editor.onconfirm = ({ data }) => {
-
                 const { crop, size, filter, color, colorMatrix, markup } = data;
 
                 // create new metadata object
@@ -216,20 +223,23 @@ const plugin = _ => {
                     const initialSize = (item.getMetadata('resize') || {}).size;
                     const targetSize = {
                         width: size.width,
-                        height: size.height
-                    }
-        
-                    if (!(targetSize.width && targetSize.height) && initialSize) {
+                        height: size.height,
+                    };
+
+                    if (
+                        !(targetSize.width && targetSize.height) &&
+                        initialSize
+                    ) {
                         targetSize.width = initialSize.width;
                         targetSize.height = initialSize.height;
                     }
-        
+
                     if (targetSize.width || targetSize.height) {
                         metadata.resize = {
                             upscale: size.upscale,
                             mode: size.mode,
-                            size: targetSize
-                        }
+                            size: targetSize,
+                        };
                     }
                 }
 
@@ -248,18 +258,21 @@ const plugin = _ => {
                 item.setMetadata(metadata);
 
                 // call
-                editor.filepondCallbackBridge.onconfirm(data, createItemAPI(item));
+                editor.filepondCallbackBridge.onconfirm(
+                    data,
+                    createItemAPI(item)
+                );
 
                 // used in instant edit mode
                 if (!handleEditorResponse) return;
+
                 editor.onclose = () => {
                     handleEditorResponse(true);
                     editor.onclose = null;
-                }
+                };
             };
 
             editor.oncancel = () => {
-
                 // call
                 editor.filepondCallbackBridge.oncancel(createItemAPI(item));
 
@@ -268,22 +281,18 @@ const plugin = _ => {
                 editor.onclose = () => {
                     handleEditorResponse(false);
                     editor.onclose = null;
-                }
-
+                };
             };
 
             editor.open(file, imageParameters);
         };
 
-
-        
         /**
          * Image Preview related
          */
 
         // create the image edit plugin, but only do so if the item is an image
         const didLoadItem = ({ root, props }) => {
-
             if (!query('GET_IMAGE_EDIT_ALLOW_EDIT')) return;
 
             const { id } = props;
@@ -297,36 +306,38 @@ const plugin = _ => {
 
             // exit if this is not an image
             if (!isPreviewableImage(file)) return;
-            
+
             // handle interactions
             root.ref.handleEdit = e => {
                 e.stopPropagation();
-                root.dispatch('EDIT_ITEM', { id })
-            }
+                root.dispatch('EDIT_ITEM', { id });
+            };
 
             if (canShowImagePreview) {
-
                 // add edit button to preview
                 const buttonView = view.createChildView(fileActionButton, {
                     label: 'edit',
                     icon: query('GET_IMAGE_EDIT_ICON_EDIT'),
-                    opacity: 0
+                    opacity: 0,
                 });
-    
+
                 // edit item classname
                 buttonView.element.classList.add('filepond--action-edit-item');
-                buttonView.element.dataset.align = query('GET_STYLE_IMAGE_EDIT_BUTTON_EDIT_ITEM_POSITION');
+                buttonView.element.dataset.align = query(
+                    'GET_STYLE_IMAGE_EDIT_BUTTON_EDIT_ITEM_POSITION'
+                );
                 buttonView.on('click', root.ref.handleEdit);
-    
-                root.ref.buttonEditItem = view.appendChildView(buttonView);
-            }
-            else {
 
+                root.ref.buttonEditItem = view.appendChildView(buttonView);
+            } else {
                 // view is file info
-                const filenameElement = view.element.querySelector('.filepond--file-info-main');
+                const filenameElement = view.element.querySelector(
+                    '.filepond--file-info-main'
+                );
                 const editButton = document.createElement('button');
                 editButton.className = 'filepond--action-edit-item-alt';
-                editButton.innerHTML = query('GET_IMAGE_EDIT_ICON_EDIT') + '<span>edit</span>';
+                editButton.innerHTML =
+                    query('GET_IMAGE_EDIT_ICON_EDIT') + '<span>edit</span>';
                 editButton.addEventListener('click', root.ref.handleEdit);
                 filenameElement.appendChild(editButton);
 
@@ -339,18 +350,19 @@ const plugin = _ => {
                 root.ref.buttonEditItem.off('click', root.ref.handleEdit);
             }
             if (root.ref.editButton) {
-                root.ref.editButton.removeEventListener('click', root.ref.handleEdit);
+                root.ref.editButton.removeEventListener(
+                    'click',
+                    root.ref.handleEdit
+                );
             }
         });
 
-        
         const routes = {
             EDIT_ITEM: openEditor,
-            DID_LOAD_ITEM: didLoadItem
+            DID_LOAD_ITEM: didLoadItem,
         };
-        
+
         if (canShowImagePreview) {
-         
             // view is file
             const didPreviewUpdate = ({ root }) => {
                 if (!root.ref.buttonEditItem) return;
@@ -358,9 +370,7 @@ const plugin = _ => {
             };
 
             routes.DID_IMAGE_PREVIEW_SHOW = didPreviewUpdate;
-        }
-        else {
-
+        } else {
         }
 
         // start writing
@@ -374,7 +384,10 @@ const plugin = _ => {
             allowImageEdit: [true, Type.BOOLEAN],
 
             // location of processing button
-            styleImageEditButtonEditItemPosition: ['bottom center', Type.STRING],
+            styleImageEditButtonEditItemPosition: [
+                'bottom center',
+                Type.STRING,
+            ],
 
             // open editor when image is dropped
             imageEditInstantEdit: [false, Type.BOOLEAN],
@@ -383,18 +396,24 @@ const plugin = _ => {
             imageEditAllowEdit: [true, Type.BOOLEAN],
 
             // the icon to use for the edit button
-            imageEditIconEdit: ['<svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M8.5 17h1.586l7-7L15.5 8.414l-7 7V17zm-1.707-2.707l8-8a1 1 0 0 1 1.414 0l3 3a1 1 0 0 1 0 1.414l-8 8A1 1 0 0 1 10.5 19h-3a1 1 0 0 1-1-1v-3a1 1 0 0 1 .293-.707z" fill="currentColor" fill-rule="nonzero"/></svg>', Type.STRING],
+            imageEditIconEdit: [
+                '<svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M8.5 17h1.586l7-7L15.5 8.414l-7 7V17zm-1.707-2.707l8-8a1 1 0 0 1 1.414 0l3 3a1 1 0 0 1 0 1.414l-8 8A1 1 0 0 1 10.5 19h-3a1 1 0 0 1-1-1v-3a1 1 0 0 1 .293-.707z" fill="currentColor" fill-rule="nonzero"/></svg>',
+                Type.STRING,
+            ],
 
             // editor object
-            imageEditEditor: [null, Type.OBJECT]
-        }
+            imageEditEditor: [null, Type.OBJECT],
+        },
     };
 };
 
 // fire pluginloaded event if running in browser, this allows registering the plugin when using async script tags
-const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+const isBrowser =
+    typeof window !== 'undefined' && typeof window.document !== 'undefined';
 if (isBrowser) {
-    document.dispatchEvent(new CustomEvent('FilePond:pluginloaded', { detail: plugin }));
+    document.dispatchEvent(
+        new CustomEvent('FilePond:pluginloaded', { detail: plugin })
+    );
 }
 
 export default plugin;
